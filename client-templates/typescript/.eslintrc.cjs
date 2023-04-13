c512888272819893481b3ce78417a97d07766a63
Toni Dahl <toni.dahl@solita.fi>
2023-04-13 22:49:38 +0300

    Add initial eslint config

---
+++ client-templates/typescript/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+    ],
+    env: {
+        node: true,
+        es2022: true,
+    },
+    rules: {
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+    },
+    settings: {
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+    },
+};
